79461ada461c2f58d813341099b43678d78e1313
Felipe Rojas <feli.rojasr@duocuc.cl>
2024-09-22 09:53:27 -0300

    Se bosqueja vista cambiar clave con su lógica temporal correspondiente.

--- src/app/cambiarclave/cambiarclave.page.ts
+++ src/app/cambiarclave/cambiarclave.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cambiarclave',
@@ -7,7 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CambiarclavePage implements OnInit {
 
-  constructor() { }
+  claveActual: string = "";
+  nuevaClave: string = "";
+  confirmarClave: string = "";
+
+  claveTemporal: string = "MiClav3";
+
+  constructor(private alertController: AlertController) { }
+
+  async onSubmit() {
+    if (this.claveActual !== this.claveTemporal){
+      this.mostrarMensaje("La clave actual es incorrecta");
+      return;
+    }
+
+    if (this.nuevaClave !== this.confirmarClave){
+      this.mostrarMensaje("Escriba la misma clave en confirmación");
+      return;
+    }
+
+    this.claveTemporal = this.nuevaClave;
+
+    this.mostrarMensaje("Clave cambiada correctamente")
+  }
+
+  async mostrarMensaje(mensaje: string) {
+    const alert = await this.alertController.create({
+      message: mensaje,
+      buttons: ["Ok"]
+    });
+
+    await alert.present();
+  }
 
   ngOnInit() {
   }
