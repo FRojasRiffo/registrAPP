85d66a887f41ae4068518fa019375f029e728706
Felipe Rojas <feli.rojasr@duocuc.cl>
2024-09-22 09:29:01 -0300

    Se agrega animación a menú de página principal.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,38 @@
-import { Component } from '@angular/core';
-import { Route, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  nombreUsuario: string = "Usuario1";
+  nombreUsuario = "Usuario1";
 
-  constructor(private router:Router) { }
+  constructor(private animationCtrl: AnimationController, private router:Router) {}
+
+  ngOnInit() {
+    this.animateItems();
+  }
+
+  animateItems() {
+    const items = document.querySelectorAll('ion-item');
+
+    items.forEach((item, index) => {
+      const animation = this.animationCtrl
+        .create()
+        .addElement(item)
+        .duration(750)
+        .fromTo('transform', 'translateX(-100%)', 'translateX(0)')
+        .delay(index * 100);
+
+      animation.play();
+    });
+  }
 
   cerrarSesion() {
     this.router.navigate(['/login'])
   }
-
 }
